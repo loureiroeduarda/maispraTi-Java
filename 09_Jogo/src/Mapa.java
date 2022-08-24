845cead46faae1578dd3f04ae784c2e737c855c4
loureiroeduarda <adv.eduardaloureiro@gmail.com>
2022-08-24 19:45:34 -0300

    Método imprimiLinha alterado

--- 09_Jogo/src/Mapa.java
+++ 09_Jogo/src/Mapa.java
@@ -1,21 +1,26 @@
-import java.util.Arrays;
-
 public class Mapa {
-    int [][] mapaInimigo;
+    int[][] mapaInimigo;
 
     public Mapa() {
         this.mapaInimigo = new int[10][10];
     }
 
-    public void imprimiLinha(int[] linha){
+    public void imprimiLinha(int[] linha) {
         String strLinha = "";
-        for(int item : linha){
-            strLinha += item;
+        for (int item : linha) {
+            if (item == 0) {
+                strLinha += "." + " ";
+            } else if (item == 1) {
+                strLinha += "X" + " ";
+            } else if (item == 2) {
+                strLinha += "*" + " ";
+            }
         }
         System.out.println(strLinha);
     }
+
     public void imprimiMapa() {
-        for (int[] linha : this.mapaInimigo){
+        for (int[] linha : this.mapaInimigo) {
             this.imprimiLinha(linha);
         }
     }
